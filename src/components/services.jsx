e5032b2e5bd6b8714132d1749981ab922e37b208
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-10-27 15:13:48 +0200

    Initial commit

---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+            App solution to close the infrastructural and technological gaps in
+            terms of management, structure and setting up of e mobility charging
+            infrastructure.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
